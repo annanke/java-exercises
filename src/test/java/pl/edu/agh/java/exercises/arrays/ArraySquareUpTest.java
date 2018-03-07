d1162f1f4de8c4d75a93ce16e7d9b59ca7b035cb
annami <amm3@wp.pl>
2018-03-07 22:00:05 +0100

    poprawka testu dla zadania ArraySquareUp - blednego uzycia assertEquals

--- src/test/java/pl/edu/agh/java/exercises/arrays/ArraySquareUpTest.java
+++ src/test/java/pl/edu/agh/java/exercises/arrays/ArraySquareUpTest.java
@@ -11,30 +11,29 @@ import java.util.Collection;
 
 @RunWith(Parameterized.class)
 public class ArraySquareUpTest {
-    private ArraySquareUp subject;
-    private int[] expectedArray;
-    private int n;
+	private ArraySquareUp subject;
+	private int[] expectedArray;
+	private int n;
 
-    @Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {0, new int[]{}},
-                {1, new int[]{1}},
-                {2, new int[]{0, 1, 2, 1}},
-                {3, new int[]{0, 0, 1, 0, 2, 1, 3, 2, 1}},
-                {4, new int[]{0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1}},
-                {5, new int[]{0, 0, 0, 0, 1, 0, 0, 0, 2, 1, 0, 0, 3, 2, 1, 0, 4, 3, 2, 1, 5, 4, 3, 2, 1}},
-        });
-    }
+	@Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] { { 0, new int[] {} }, { 1, new int[] { 1 } },
+				{ 2, new int[] { 0, 1, 2, 1 } }, { 3, new int[] { 0, 0, 1, 0, 2, 1, 3, 2, 1 } },
+				{ 4, new int[] { 0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1 } },
+				{ 5, new int[] { 0, 0, 0, 0, 1, 0, 0, 0, 2, 1, 0, 0, 3, 2, 1, 0, 4, 3, 2, 1, 5, 4, 3, 2, 1 } }, });
+	}
 
-    public ArraySquareUpTest(int n, int[] expectedArray) {
-        this.n = n;
-        this.expectedArray = expectedArray;
-        this.subject = new ArraySquareUp();
-    }
+	public ArraySquareUpTest(int n, int[] expectedArray) {
+		this.n = n;
+		this.expectedArray = expectedArray;
+		this.subject = new ArraySquareUp();
+	}
 
-    @Test
-    public void testSquareUp() {
-        Assert.assertArrayEquals("Invalid result for: " + n, expectedArray, subject.squareUp(n));
-    }
+	@Test
+	public void testSquareUp() {
+		// Assert.assertEquals("Invalid result for: " + n, expectedArray, subject.squareUp(n));
+		// metoda assertEquals w tym wypadku blednie porownanuje obiekty - przez
+		// referencje a nie wartosci. W tym teście nie jest możliwe spełnienie takiej równości
+		Assert.assertArrayEquals(expectedArray, subject.squareUp(n));
+	}
 }
